04e76033ad88b39deb085a4903a70bac88ddc295
Leonardo Paul Carrillo Arce <leonardo@MacBook-Pro-de-Leonardo.local>
2024-06-26 18:44:41 -0500

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -8,20 +7,6 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent {
   title = 'portfolio-frontend-yavirac';
-  response = ""
-  constructor(private http: HttpClient) { }
 
-  getHello() {
-    this.http.get<any>('https://demo-backend-cg46.onrender.com/')
-      .subscribe(
-        (response) => {
-          console.log('Respuesta del servidor:', response);
-          this.response = response;
-        },
-        (error) => {
-          console.error('Error al obtener el mensaje:', error);
-        
-        }
-      );
-  }
+  
 }
