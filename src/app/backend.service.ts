ec93200a291b0883204354a263bc66f1f78e4ac3
Leonardo Paul Carrillo Arce <leonardo@MacBook-Pro-de-Leonardo.local>
2024-06-27 08:23:44 -0500

    idk

--- src/app/backend.service.ts
+++ src/app/backend.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class BackendService {
-  private apiUrl = 'https://demo-backend-cg46.onrender.com'; // URL del backend Nest.js
+  private apiUrl = 'https://demo-backend-cg46.onrender.com'; 
 
   constructor() {}
 
